35f34eebcd55aa7df28575d3cd9ccb23c803f68d
Marcos Antônio Ferreira <marcos.a.ferreira@gmail.com>
2022-02-05 17:43:07 -0300

    feat: add MainViewController.swift

---
+++ Main/MainViewController.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+class MainViewController: NSViewController {
+	override func loadView() {
+		view = NSView()
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+}
