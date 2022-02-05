155cad6f9be20f8d6a0427be738d07efe212e7bc
Marcos Antônio Ferreira <marcos.a.ferreira@gmail.com>
2022-02-05 17:29:07 -0300

    chore: setup a starter project

---
+++ Main/AppDelegate.swift
@@ -0,0 +1,18 @@
+
+// 05/02/22.
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {}
+
+	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
+}
+
