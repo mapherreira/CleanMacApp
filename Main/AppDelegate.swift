25ceeec0b1f95a22219aa7429145e1c5413440ae
Marcos Antônio Ferreira <marcos.a.ferreira@gmail.com>
2022-02-05 17:54:02 -0300

    feat: finish MainWindow open behavior

--- Main/AppDelegate.swift
+++ Main/AppDelegate.swift
@@ -6,7 +6,8 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		
+		let coordinator = MainCoordinator()
+		coordinator.start()
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {}
--- Main/MainWindowController.swift
+++ Main/MainWindowController.swift
@@ -7,5 +7,4 @@ class MainWindowController: NSWindowController {
 	
 		
 	}
-
 }
